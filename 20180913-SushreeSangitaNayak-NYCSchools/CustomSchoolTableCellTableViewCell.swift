2dead3e3b15332b43ac743fdb3ffd2c775c9acc9
Sangita <sangita.munu@gmail.com>
2018-09-15 22:29:27 -0400

    commit no 1

---
+++ 20180913-SushreeSangitaNayak-NYCSchools/CustomSchoolTableCellTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  CustomSchoolTableCellTableViewCell.swift
+//  20180913-SushreeSangitaNayak-NYCSchools
+//
+//  Created by Sangita on 9/15/18.
+//  Copyright © 2018 Sangita. All rights reserved.
+//
+
+import UIKit
+
+class CustomSchoolTableCellTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var schoolNameLbl: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
